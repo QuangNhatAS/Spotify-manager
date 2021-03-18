a9f56aa3440ce23477878c8eaeb6fa7eda337317
Quang Nhat <doanquangnhat97@gmail.com>
2021-03-18 08:30:43 +0700

    Adđ DataLayer model

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Login from './Login/Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from './Player/Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{user}, dispatch] = useDataLayerValue();
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = '';
@@ -17,7 +19,12 @@ function App() {
     if(_token){
       setToken(_token);
       spotify.setAccessToken(_token);
-      spotify.getMe().then(user => console.log(user))
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      })
     }
   }, [])
   return (
--- src/Player/Player.js
+++ src/Player/Player.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Player() {
   return (
     <div>
-      <h1>This is player has been log in...</h1>
+      <h1>Player has been log in...</h1>
     </div>
   )
 }
